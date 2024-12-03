b00cbe5a94ef18d2a9d0399b86273aac25067041
sungyo4869 <hchengjian1@gmail.com>
2024-12-04 00:32:45 +0900

    /ws/matching、WebSocket通信してないけど形だけかえすようになった

--- handler/matching.go
+++ handler/matching.go
@@ -2,28 +2,62 @@ package handler
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
+	"github.com/sugyo4869/HackU_KOSEN_2024/model"
 	"github.com/sugyo4869/HackU_KOSEN_2024/service"
 )
 
 type MatchingHandler struct {
-	svc *service.SelectedCardService
+	SCSvc *service.SelectedCardService
+	RmSvc *service.RoomService
 }
 
-func NewMatchingHandler(svc *service.SelectedCardService) *MatchingHandler {
+func NewMatchingHandler(SCSvc *service.SelectedCardService, RmSvc *service.RoomService) *MatchingHandler {
 	return &MatchingHandler{
-		svc: svc,
+		SCSvc: SCSvc,
+		RmSvc: RmSvc,
 	}
 }
 
 func (h *MatchingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var res model.MatchingWSResponse
 
-	res, err := h.svc.ReadSelectedCard(r.Context(), 1)
+	cards, err := h.SCSvc.ReadSelectedCard(r.Context(), 1)
 	if err != nil {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
+		log.Println("matching: err = ", err)
+		return
 	}
 
+	var player model.Player
+
+	player.SelectedCards = cards
+	player.Username = "user1"
+	res.Players = append(res.Players, player)
+
+	cards, err = h.SCSvc.ReadSelectedCard(r.Context(), 1)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		log.Println("matching: err = ", err)
+		return
+	}
+
+	player.SelectedCards = cards
+	player.Username = "user1"
+
+	res.Players = append(res.Players, player)
+
+	room, err := h.RmSvc.CreateRoom(r.Context(), []int{1,2})
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		log.Println("matching: err = ", err)
+		return
+	}
+
+	res.RoomId = room.RoomId
+	
 	err = json.NewEncoder(w).Encode(res)
 	if err != nil {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
