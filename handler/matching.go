488b653bc49049bde8d24e943a2eb9fbfc2fcc03
sungyo4869 <hchengjian1@gmail.com>
2024-12-03 23:04:09 +0900

    user1の手札一覧を返すコントローラーを実装した

--- handler/matching.go
+++ handler/matching.go
@@ -1,18 +1,32 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
 
-	"github.com/sugyo4869/HackU_KOSEN_2024/model"
+	"github.com/sugyo4869/HackU_KOSEN_2024/service"
 )
 
-type MatchingHandler struct{}
-
-func NewMatchingHandler() *MatchingHandler {
-	return &MatchingHandler{}
+type MatchingHandler struct {
+	svc *service.SelectedCardService
 }
 
-func (h *MatchingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) *model.MatchingWSResponse {
+func NewMatchingHandler(svc *service.SelectedCardService) *MatchingHandler {
+	return &MatchingHandler{
+		svc: svc,
+	}
+}
 
-	return &model.MatchingWSResponse{}
+func (h *MatchingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	res, err := h.svc.ReadSelectedCard(r.Context(), 1)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+	}
+
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+	}
+
 }
