5a814b444367be063704a1c810d6a96758f960fa
sungyo4869 <hchengjian1@gmail.com>
2024-12-10 21:59:11 +0900

    battlesのテーブルを定義した

--- db/schema.sql
+++ db/schema.sql
@@ -33,6 +33,29 @@ CREATE TABLE IF NOT EXISTS rooms (
     FOREIGN KEY (user2_id) REFERENCES users(id)
 );
 
+CREATE TABLE IF NOT EXISTS battles (
+    battle_id INT NOT NULL AUTO_INCREMENT,
+    room_id INT NOT NULL,
+    user_id INT NOT NULL,
+    fire_card_id INT,
+    water_card_id INT,
+    grass_card_id INT,
+    kamekame_card_id INT,
+    nankuru_card_id INT,
+    random_card_id INT,
+    hp INT NOT NULL,
+    result ENUM('win', 'lose', 'draw', 'pending') NOT NULL,
+    PRIMARY KEY (battle_id),
+    FOREIGN KEY (room_id) REFERENCES rooms(room_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (fire_card_id) REFERENCES cards(id),
+    FOREIGN KEY (water_card_id) REFERENCES cards(id),
+    FOREIGN KEY (grass_card_id) REFERENCES cards(id),
+    FOREIGN KEY (kamekame_card_id) REFERENCES cards(id),
+    FOREIGN KEY (nankuru_card_id) REFERENCES cards(id),
+    FOREIGN KEY (random_card_id) REFERENCES cards(id)
+);
+
 INSERT INTO
     users (username, password)
 VALUES
@@ -43,56 +66,77 @@ VALUES
 INSERT INTO
     cards (user_id, picture, card_name)
 VALUES
-(1, 'card1.jpg', 'カード1'),
-(1, 'card2.jpg', 'カード2'),
-(1, 'card3.jpg', 'カード3'),
-(1, 'card4.jpg', 'カード4'),
-(1, 'card5.jpg', 'カード5'),
-(1, 'card6.jpg', 'カード6'),
-(1, 'card7.jpg', 'カード7'),
-(1, 'card8.jpg', 'カード8'),
-(1, 'card9.jpg', 'カード9'),
-(1, 'card10.jpg', 'カード10'),
-
-(2, 'card11.jpg', 'カード11'),
-(2, 'card12.jpg', 'カード12'),
-(2, 'card13.jpg', 'カード13'),
-(2, 'card14.jpg', 'カード14'),
-(2, 'card15.jpg', 'カード15'),
-(2, 'card16.jpg', 'カード16'),
-(2, 'card17.jpg', 'カード17'),
-(2, 'card18.jpg', 'カード18'),
-(2, 'card19.jpg', 'カード19'),
-(2, 'card20.jpg', 'カード20'),
-
-(3, 'card21.jpg', 'カード21'),
-(3, 'card22.jpg', 'カード22'),
-(3, 'card23.jpg', 'カード23'),
-(3, 'card24.jpg', 'カード24'),
-(3, 'card25.jpg', 'カード25'),
-(3, 'card26.jpg', 'カード26'),
-(3, 'card27.jpg', 'カード27'),
-(3, 'card28.jpg', 'カード28'),
-(3, 'card29.jpg', 'カード29'),
-(3, 'card30.jpg', 'カード30');
+    (1, 'card1.jpg', 'カード1'),
+    (1, 'card2.jpg', 'カード2'),
+    (1, 'card3.jpg', 'カード3'),
+    (1, 'card4.jpg', 'カード4'),
+    (1, 'card5.jpg', 'カード5'),
+    (1, 'card6.jpg', 'カード6'),
+    (1, 'card7.jpg', 'カード7'),
+    (1, 'card8.jpg', 'カード8'),
+    (1, 'card9.jpg', 'カード9'),
+    (1, 'card10.jpg', 'カード10'),
+    (2, 'card11.jpg', 'カード11'),
+    (2, 'card12.jpg', 'カード12'),
+    (2, 'card13.jpg', 'カード13'),
+    (2, 'card14.jpg', 'カード14'),
+    (2, 'card15.jpg', 'カード15'),
+    (2, 'card16.jpg', 'カード16'),
+    (2, 'card17.jpg', 'カード17'),
+    (2, 'card18.jpg', 'カード18'),
+    (2, 'card19.jpg', 'カード19'),
+    (2, 'card20.jpg', 'カード20'),
+    (3, 'card21.jpg', 'カード21'),
+    (3, 'card22.jpg', 'カード22'),
+    (3, 'card23.jpg', 'カード23'),
+    (3, 'card24.jpg', 'カード24'),
+    (3, 'card25.jpg', 'カード25'),
+    (3, 'card26.jpg', 'カード26'),
+    (3, 'card27.jpg', 'カード27'),
+    (3, 'card28.jpg', 'カード28'),
+    (3, 'card29.jpg', 'カード29'),
+    (3, 'card30.jpg', 'カード30');
 
 INSERT INTO
     user_selected (user_id, card_id)
 VALUES
-(1,2),
-(1,3),
-(1,5),
-(1,7),
-(1,9),
+    (1, 2),
+    (1, 3),
+    (1, 5),
+    (1, 7),
+    (1, 9),
+    (2, 11),
+    (2, 12),
+    (2, 14),
+    (2, 16),
+    (2, 18),
+    (3, 20),
+    (3, 21),
+    (3, 23),
+    (3, 25),
+    (3, 27);
 
-(2, 11),
-(2, 12),
-(2, 14),
-(2, 16),
-(2, 18),
+INSERT INTO
+    rooms(user1_id, user2_id)
+VALUES
+    (1, 2),
+    (1, 2);
 
-(3, 20),
-(3, 21),
-(3, 23),
-(3, 25),
-(3, 27);
+INSERT INTO
+    battles (
+        room_id,
+        user_id,
+        fire_card_id,
+        water_card_id,
+        grass_card_id,
+        kamekame_card_id,
+        nankuru_card_id,
+        random_card_id,
+        hp,
+        result
+    )
+VALUES
+    (3, 1, 2, 3, 4, 5, 6, 7, 3, 'win'),
+    (3, 2, 13, 14, 15, 16, 17, 18, 0, 'lose'),
+    (4, 1, 4, 5, 6, 7, 8, 9, 1, 'draw'),
+    (4, 2, 14, 15, 16, 17, 18, 19, 1, 'draw');
